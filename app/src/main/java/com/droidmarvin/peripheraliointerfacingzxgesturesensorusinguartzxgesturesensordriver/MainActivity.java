cad1dc7a58b592b94dd6871799aeb7381a284f26
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 12:01:38 +0300

    set up the sensor and listen for gesture event

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingzxgesturesensorusinguartzxgesturesensordriver/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingzxgesturesensorusinguartzxgesturesensordriver/MainActivity.java
@@ -2,11 +2,47 @@ package com.droidmarvin.peripheraliointerfacingzxgesturesensorusinguartzxgesture
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.things.contrib.driver.zxgesturesensor.ZXGestureSensor;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+
+    private static final String ZX_GESTURE_SENSOR = "UART0";
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ZXGestureSensor mZxSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
     }
+
+    // Access the Gesture sensor and listen for gesture event
+    private void mmZxGestureSetup(){
+
+        try {
+            mZxSensor = ZXGestureSensor.getUartSensor(ZX_GESTURE_SENSOR, new Handler());
+            mZxSensor.setListener(new ZXGestureSensor.OnGestureEventListener() {
+                @Override
+                public void onGestureEvent(ZXGestureSensor sensor, ZXGestureSensor.Gesture gesture, int param) {
+                    // do something awesome
+                }
+            });
+        }catch (IOException e){
+            // couldn't configure the sensor...
+        }
+
+
+    }
+
+
+
 }
